2451b778b15fbad4042f7c33e5124051ba8b8536
User <User@LAB2-10>
2021-10-25 14:46:24 +0300

    iii

--- IDA.App/ViewModels/LogInViewModels.cs
+++ IDA.App/ViewModels/LogInViewModels.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace IDA.App.ViewModels
 {
-    class LogInViewModels
+  public  class LogInViewModels:ViewModelBase
     {
+        private int entryEmail;
+        public int EntryEmail { get { return entryEmail; } set { entryEmail = value; OnPropertyChanged("EntryEmail"); } }
+
+        private int entryPass;
+        public int EntryPass { get { return entryPass; } set { entryPass = value; OnPropertyChanged("EntryPass"); } }
+
+
+        public ICommand LogInCommand { protected set; get; }
+        public ICommand RegisterCommand { protected set; get; }
+      
+
     }
 }

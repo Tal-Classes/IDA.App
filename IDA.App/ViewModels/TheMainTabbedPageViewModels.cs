0529e5b76ae4d0cc6f69b31e878a3bf5e3b72f06
User <User@LAB2-10>
2021-10-31 08:57:30 +0200

    rr

--- IDA.App/ViewModels/TheMainTabbedPageViewModels.cs
+++ IDA.App/ViewModels/TheMainTabbedPageViewModels.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
+using IDA.App.Models;
+using IDA.App.Views;
+using Xamarin.Forms;
+
 
 namespace IDA.App.ViewModels
 {
